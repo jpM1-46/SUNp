a857da37cc141f6537daf52d3486ef4ee482eb69
jpM1-46 <82129096+jpM1-46@users.noreply.github.com>
2022-06-09 11:29:56 +0900

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -25,7 +25,7 @@ class CarInterface(CarInterfaceBase):
     ret.carName = "toyota"
     ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.toyota)]
 
-    ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
+    ret.steerActuatorDelay = -0.03  # Default delay, Prius has larger delay
     ret.steerLimitTimer = 0.4
 
     ret.stoppingControl = False  # Toyota starts braking more when it thinks you want to stop
